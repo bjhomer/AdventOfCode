5b244afaeebb306d68ad4e54214205b09b085f99
BJ Homer <bjhomer@gmail.com>
2021-01-06 21:24:42 -0700

    Done with 2020-16

--- Sources/AdventOfCode2020/Day16.swift
+++ Sources/AdventOfCode2020/Day16.swift
@@ -9,7 +9,7 @@ import Foundation
 import AdventCore
 import Algorithms
 
-private let sampleInput = """
+private let sampleInput1 = """
 class: 1-3 or 5-7
 row: 6-11 or 33-44
 seat: 13-40 or 45-50
@@ -24,8 +24,22 @@ nearby tickets:
 38,6,12
 """
 
+private let sampleInput2 = """
+class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+"""
+
 func day16(input inputData: Data) {
-//    let input = sampleInput
+//    let input = sampleInput2
     let input = String(decoding: inputData, as: UTF8.self)
 
     let (rulesStr, ticketStr, nearbyStr) = input
@@ -43,17 +57,41 @@ func day16(input inputData: Data) {
 
     let part1 = invalidValues.reduce(0, +)
     print("Part 1: \(part1)")
+
+    let validTickets = nearbyTickets.filter { rules.isValidTicket($0) }
+
+    let ruleAssignments = assignRulePositions(rules, using: validTickets)
+    print("Assigned rules:", ruleAssignments.map(\.name) )
+
+    myTicket.print(using: ruleAssignments)
+
+    let part2 = myTicket
+        .fields(using: ruleAssignments)
+        .filter { (name, value) in name.hasPrefix("departure") }
+        .map { (name, value) in value }
+        .reduce(1, *)
+
+    print("Part 2: \( part2 )")
 }
-
-
 
 extension Array where Element == Rule {
     func anyAccepts(_ value: Int) -> Bool {
         self.contains(where: { $0.accepts(value) } )
     }
+
+    func anyAccepts(_ values: [Int]) -> Bool {
+        self.contains(where: { $0.accepts(values) })
+    }
+
+    func isValidTicket(_ ticket: Ticket) -> Bool {
+        for value in ticket.values {
+            if anyAccepts(value) == false { return false }
+        }
+        return true
+    }
 }
 
-private struct Rule {
+private struct Rule: Hashable {
     var name: String
     var validRanges: [ClosedRange<Int>]
 
@@ -77,13 +115,77 @@ private struct Rule {
     func accepts(_ value: Int) -> Bool {
         return validRanges.contains(where: { $0.contains(value) } )
     }
+
+    func accepts(_ values: [Int]) -> Bool {
+        return values.allSatisfy { self.accepts($0) }
+    }
+}
+
+private func assignRulePositions(_ rules: [Rule], using tickets: [Ticket]) -> [Rule] {
+    var possibleAssignments: [Int: Set<Rule>] = [:]
+    var availableRules = Set(rules)
+    var assignments: [Int: Rule] = [:]
+
+    let fields = Array(0..<rules.count)
+
+    for i in 0..<rules.count {
+        possibleAssignments[i] = Set(rules)
+    }
+
+    while availableRules.isEmpty == false {
+        for field in fields {
+            var fieldPossibilities: Set<Rule> {
+                get { possibleAssignments[field]! }
+                set { possibleAssignments[field] = newValue }
+            }
+
+            let invalidRules = tickets.invalidRules(from: rules, forField: field)
+            fieldPossibilities.subtract(invalidRules)
+
+            if fieldPossibilities.count == 1 {
+                // We found a match!
+                let rule = fieldPossibilities.first!
+                assignments[field] = rule
+                availableRules.remove(rule)
+                for otherField in fields where otherField != field {
+                    possibleAssignments[otherField]!.remove(rule)
+                }
+            }
+        }
+    }
+
+
+    let sortedRules = assignments.keys.sorted().map { assignments[$0]! }
+    return sortedRules
+
 }
 
 
-private struct Ticket {
+private struct Ticket: Hashable {
     var values: [Int]
 
     init<Str>(line: Str) where Str: StringProtocol {
         values = line.split(separator: ",").compactMap { Int($0) }
+    }
+
+    func print(using rules: [Rule]) {
+        let pairs = zip(rules.map(\.name), values)
+
+        for x in pairs {
+            Swift.print("  \(x.0) -> \(x.1)")
+        }
+    }
+
+    func fields(using rules: [Rule]) -> [String: Int] {
+        Dictionary(uniqueKeysWithValues: zip(rules.map(\.name), values) )
+    }
+}
+
+
+extension Array where Element == Ticket {
+    func invalidRules(from rules: [Rule], forField field: Int) -> [Rule] {
+        let allValues = self.map { $0.values[field] }
+        let invalidRules = rules.filter { $0.accepts(allValues) == false }
+        return invalidRules
     }
 }
