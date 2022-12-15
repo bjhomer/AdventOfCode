8f44c72ae45c0be194261604f8d86386a368d96f
BJ Homer <bjhomer@gmail.com>
2022-12-14 23:52:59 -0700

    Make Grid support arbitrary positoins

--- Sources/AdventCore/Grid.swift
+++ Sources/AdventCore/Grid.swift
@@ -7,58 +7,116 @@
 
 import Algorithms
 
-public struct Grid<T> {
-    public struct Index: Hashable, Codable, CustomStringConvertible {
-        public var r: Int
-        public var c: Int
+public struct GridPoint: Hashable, Codable, CustomStringConvertible {
 
-        public init(r: Int, c: Int) {
-            self.r = r
-            self.c = c
-        }
+    public var r: Int
+    public var c: Int
 
-        func offset(by delta: (r: Int, c: Int)) -> Index {
-            return Index(r: r + delta.r, c: c + delta.c)
-        }
-
-        public var description: String {
-            "(\(r), \(c))"
-        }
+    public var x: Int {
+        get { c }
+        set { c = newValue }
     }
 
+    public var y: Int {
+        get { r }
+        set { r = newValue}
+    }
+
+    public init(r: Int, c: Int) {
+        self.r = r
+        self.c = c
+    }
+
+    public init(x: Int, y: Int) {
+        self.r = y
+        self.c = x
+    }
+
+    func offset(by delta: (r: Int, c: Int)) -> Self {
+        return Self(r: r + delta.r, c: c + delta.c)
+    }
+
+    public func offset(x: Int, y: Int) -> Self {
+        var copy = self
+        copy.x += x
+        copy.y += y
+        return copy
+    }
+
+    public func offset(r: Int, c: Int) -> Self {
+        var copy = self
+        copy.r += r
+        copy.c += c
+        return copy
+    }
+
+    public var description: String {
+        "(r:\(r), c:\(c))"
+    }
+}
+
+public struct Grid<T> {
+    public typealias Index = GridPoint
+
+    public let width: Int
+    public let height: Int
+
+    public var minX: Int { topLeft.x }
+    public var maxX: Int { topLeft.x + width - 1 }
+    public var minC: Int { minX }
+    public var maxC: Int { maxX }
+
+    public var minY: Int { topLeft.y }
+    public var maxY: Int { topLeft.y + height - 1 }
+    public var minR: Int { minY }
+    public var maxR: Int { maxY }
+
+    private var topLeft = GridPoint(r: 0, c: 0)
     private var rows: [[T]]
 
     public init(rows: [[T]]) {
+        width = rows[0].count
+        height = rows.count
         self.rows = rows
     }
 
     public init(_ lines: some Sequence<some Sequence<T>>) {
-        self.rows = Array(lines.map { Array($0) })
+        self.init(rows: Array(lines.map { Array($0) }))
     }
 
-    public var width: Int { rows[0].count }
-    public var height: Int { rows.count }
+    public init(topLeft: GridPoint, bottomRight: GridPoint, defaultValue: T) {
+        assert(topLeft.x <= bottomRight.x)
+        assert(topLeft.y <= bottomRight.y)
+
+        self.topLeft = topLeft
+
+        width = bottomRight.x - topLeft.x + 1
+        height = bottomRight.y - topLeft.y + 1
+
+        let defaultRow = Array(repeating: defaultValue, count: width)
+        rows = Array(repeating: defaultRow, count: height)
+    }
 
     public var indices: [Index] {
-        return Array(product(0..<height, 0..<width).map { Index(r: $0.0, c: $0.1) })
+        return Array(product(minR..<maxR, minC..<maxC).map { Index(r: $0.0, c: $0.1) })
     }
 
     public subscript(row r: Int, column c: Int) -> T {
-        get { rows[r][c] }
-        set { rows[r][c] = newValue }
+        get { rows[r-minR][c-minC] }
+        set { rows[r-minR][c-minC] = newValue }
     }
 
     public subscript(column c: Int) -> [T] {
-        rows.map { $0[c] }
+        rows.map { $0[c-minC] }
     }
 
     public subscript(row r: Int) -> [T] {
-        rows[r]
+        rows[r-minR]
     }
 
     public subscript(_ index: Index) -> T {
-        get { rows[index.r][index.c] }
-        set { rows[index.r][index.c] = newValue }
+        get { self[row: index.r, column: index.c] }
+        set { self[row: index.r, column: index.c] = newValue }
     }
 
     public enum Direction: CaseIterable, Equatable {
@@ -76,7 +134,7 @@ public struct Grid<T> {
 
     public func index(moved: Direction, from start: Index) -> Index? {
         let index = start.offset(by: moved.offsets)
-        guard (0..<height) ~= index.r && (0..<width) ~= index.c else { return nil }
+        guard (minR..<maxR) ~= index.r && (minR..<maxR) ~= index.c else { return nil }
         return index
     }
 
@@ -93,6 +151,6 @@ extension Grid where T: Equatable {
 
 extension Grid: CustomStringConvertible where T == Character {
     public var description: String {
-        rows.map { String($0) }.joined(separator: "\n")
+        zip((minR)...(maxR), rows).map { "\(String($0).padding(toLength: 3, withPad: " ", startingAt: 0)) \(String($1))" }.joined(separator: "\n")
     }
 }
---
+++ Sources/AdventOfCode2022/Day15.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by BJ Homer on 12/14/22.
+//
+
+import Foundation
